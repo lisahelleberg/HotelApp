a266e1b46e28c386380c947d7343dcb7017ad359
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2017-04-03 14:47:35 +0200

    Har tilføjet tekst i klasserne 'Guest' og 'GuestCatalogSingleton'

--- HotelApp/Model/GuestCatalogSingleton.cs
+++ HotelApp/Model/GuestCatalogSingleton.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace HotelApp.Model
 {
-    class GuestCatalogSingleton
+    public sealed class GuestCatalogSingleton
     {
+        private static GuestCatalogSingleton instance = new GuestCatalogSingleton();
+        public static GuestCatalogSingleton SingletonInstance
+        {
+            get { return instance; }
+        }
+        public Guest SingletonFile { get; set; }
+        public ObservableCollection<Guest> GuestList { get; set; }
+
+        public GuestCatalogSingleton()
+        {
+            GuestList = new ObservableCollection<Guest>();
+        }
     }
 }
